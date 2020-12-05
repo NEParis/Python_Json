671e471d82f1dcdc9a1a66f63c0a43bd8b72d591
Erik France <erik@neparis.com>
2020-12-05 08:33:46 -0600

    Yeah! Can access 1 student.

--- classRoster.py
+++ classRoster.py
@@ -1,8 +1,5 @@
 import json
 
-
-
-
 ## Initialize
 classesCollection = {}
 
@@ -12,20 +9,21 @@ with open("class_rosters.json", "r") as rosterFile:
     classesCollection = json.load(rosterFile)
     # print(classesCollection)
 
-##Access specific value
+# ## Access specific value
 # print(classesCollection['classes'][0]['students'][1]['lastName'])
 
 
-#Access one stuedent
-student = classesCollection['classes'][0]['students'][1]
-print(student)
+# ## Access one stuedent
+# student = classesCollection['classes'][0]['students'][1]
+# print(student)
 
 
-# ## Iterate collection
-# for student in classesCollection['classes'],['students']:
-#     print("-------------------")
-#     print("First Name: " + student['firstName'])
-#     print("Last Name: " + student['lastName'])
-#     print("Age: " + student['age'])
-#     print("Gender: " + student['gender'])
-#     print("-------------------")
+## Iterate collection
+for classes in classesCollection['classes']:
+    print(classes['className'],"students:")
+    for student in classes['students']:
+        print("First Name: " + student['firstName'])
+        print("Last Name: " + student['lastName'])
+        print("Age: " + student['age'])
+        print("Gender: " + student['gender'])
+        print("-------------------")
